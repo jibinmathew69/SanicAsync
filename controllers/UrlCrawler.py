c948c84c9aac5898cca53941f4380d5a89c9cf9f
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-08 19:20:35 +0530

    Add is_valid_url in UrlCrawler

--- controllers/UrlCrawler.py
+++ controllers/UrlCrawler.py
@@ -8,7 +8,17 @@ class UrlCrawler:
             config = url_config()
             self.upload_folder = config["upload_folder"]
             self.max_connect = config["max_connect"]
+            self.allowed_formats = config["allowed_formats"]
+            self.max_size = config["max_size"]
         except:
             raise ServerError("Internal Server Error",status_code=500)
 
 
+    async def is_valid_url(self,url):
+        import requests
+
+        valid_request = requests.head(url)
+        if valid_request.headers["content-type"] not in self.allowed_formats or valid_request.headers["content-length"] > 20480:
+            return False
+
+        return True
