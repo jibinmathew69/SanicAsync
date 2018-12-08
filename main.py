1e25bf4afcbd1a076fd63b486d91d6e81ddb5543
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-09 00:06:41 +0530

    Add /v1/images/upload api

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ def make_app():
     app = Sanic(__name__)
     CORS(app,automatic_options=True)
 
+    @app.route('/v1/images/upload',methods=["POST"])
+    async def upload(request):
+        from controllers.UrlCrawler import UrlCrawler
+        from controllers.Imgur import Imgur
+
+        return await UrlCrawler().fetcher(request,Imgur(),
+                                    completed=completed,
+                                    pending=pending,
+                                    failed=failed,
+                                    created=created,
+                                    finished=finished
+                                    )
+
+
+
     return app
 
 
