b322a04ab4d6c56ca3778c2c45f52eea393aac9a
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-08 16:59:24 +0530

    Add sanic main

---
+++ main.py
@@ -0,0 +1,15 @@
+from sanic import Sanic
+from sanic_cors import CORS
+
+
+def make_app():
+    app = Sanic(__name__)
+    CORS(app,automatic_options=True)
+
+    return app
+
+
+if __name__ == '__main__':
+    app = make_app()
+
+    app.run(debug=True, host='0.0.0.0', port='8001')
