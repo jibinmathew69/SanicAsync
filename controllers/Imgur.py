2b53596d23b7a2a86f5978a556d220dedec42a59
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-08 17:15:22 +0530

    Add Imgur init

--- controllers/Imgur.py
+++ controllers/Imgur.py
@@ -1,3 +1,14 @@
+from sanic import response
+from sanic.exceptions import ServerError
+from settings.settings import imgur_config
+
 class Imgur:
     def __init__(self):
-        pass
+
+        try:
+            config          = imgur_config()
+            self.url        = config["IMGURURL"]
+            self.client_id  = config["CLIENTID"]
+
+        except:
+            raise ServerError("Internal Server Error",status_code=500)
