a1dd9a9a4f9842d6c8c99dcc34560e71fee3bcb1
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-08 17:48:44 +0530

    Add image_upload to Imgur

--- controllers/Imgur.py
+++ controllers/Imgur.py
@@ -12,3 +12,28 @@ class Imgur:
 
         except:
             raise ServerError("Internal Server Error",status_code=500)
+
+
+    async def image_upload(self,file_name):
+        import aiohttp
+        import os.path
+
+        if not os.path.isfile(file_name):
+            return None
+
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                self.url,
+                data={
+                    'image' : open(file_name,'rb')
+                },
+                headers={
+                    'Authorization' : 'Client-ID {}'.format(self.client_id)
+                }
+            ) as imgur_request:
+                imgur_response = await imgur_request.json()
+                if imgur_request.status == 200:
+                    return imgur_response
+
+        return None
