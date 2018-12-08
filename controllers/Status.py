d2215dbc09309eb8da87c40686edc83ca7ea51a4
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-09 00:58:57 +0530

    Add get status by api in Status

--- controllers/Status.py
+++ controllers/Status.py
@@ -5,3 +5,17 @@ class Status:
         import itertools
         return response.json(itertools.chain.from_iterable(kwargs["completed"].values()))
 
+
+    def get_status_id(self,id,**kwargs):
+        result = {
+            "id" : id,
+            "created" : kwargs["created"][id],
+            "finished" : kwargs["finished"][id],
+            "uploaded" : {
+                "pending" : kwargs["pending"][id],
+                "completed" : kwargs["completed"][id],
+                "failed" : kwargs["failed"][id]
+            }
+        }
+
+        return response.json(result)
