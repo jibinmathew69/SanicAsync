bd20161a68743345b73bfe4876adeea6239df6cb
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-09 15:50:48 +0530

    Add exception handler in Status

--- controllers/Status.py
+++ controllers/Status.py
@@ -1,4 +1,5 @@
 from sanic import response
+from sanic.exceptions import ServerError
 class Status:
 
     def get_status(self,**kwargs):
@@ -8,26 +9,27 @@ class Status:
 
     def get_status_id(self,id,**kwargs):
 
+        try:
+            if not kwargs["finished"][id]:
+                status = "complete"
+            elif not kwargs["completed"][id] and not kwargs["failed"][id]:
+                status = "pending"
+            else:
+                status = "in-progress"
 
-        if not kwargs["finished"][id]:
-            status = "complete"
-        elif not kwargs["completed"][id] and not kwargs["failed"][id]:
-            status = "pending"
-        else:
-            status = "in-progress"
 
 
-
-        result = {
-            "id" : id,
-            "created" : kwargs["created"][id],
-            "finished" : kwargs["finished"][id],
-            "status" : status,
-            "uploaded" : {
-                "pending" : kwargs["pending"][id],
-                "completed" : kwargs["completed"][id],
-                "failed" : kwargs["failed"][id]
+            result = {
+                "id" : id,
+                "created" : kwargs["created"][id],
+                "finished" : kwargs["finished"][id],
+                "status" : status,
+                "uploaded" : {
+                    "pending" : kwargs["pending"][id],
+                    "completed" : kwargs["completed"][id],
+                    "failed" : kwargs["failed"][id]
+                }
             }
-        }
-
+        except:
+            raise ServerError("Internal Server Error",500)
         return response.json(result)
