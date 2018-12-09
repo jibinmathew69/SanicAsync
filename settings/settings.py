920620b7d4694ed78a3effd20e630623f2f3cfc5
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-09 14:45:18 +0530

    Add log settings

--- settings/settings.py
+++ settings/settings.py
@@ -21,7 +21,9 @@ def url_config():
             'max_connect'       : dotenv.get('MAXCONNECT'),
             'allowed_formats'   : ("image/png", "image/jpeg", "image/jpg","image/gif","image/apng","image/tiff"),
             'max_size'          : 2e+7,
-            'chunk_size'        : 1<<15
+            'chunk_size'        : 1<<15,
+            'log_folder'        : dotenv.get('LOGFOLDER'),
+            'log_file'          : dotenv.get('LOGFILE')
         }
         return urlconfig
     except:
